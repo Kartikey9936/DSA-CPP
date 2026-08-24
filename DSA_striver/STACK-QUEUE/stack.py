55b3c735c069f8c621e4ab24f8dbd8184e69be84
Kartikey <kartikeykesharwani9936@gmail.com>
2026-08-25 01:13:06 +0530

    added

--- DSA_striver/STACK-QUEUE/stack.py
+++ DSA_striver/STACK-QUEUE/stack.py
@@ -250,3 +250,27 @@ public:
     }
 };
 
+temperature 
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        stack<int> st;
+        vector<int> ans(n,0);
+        
+        for(int i =0;i<n;i++){
+            while(!st.empty()  && temperatures[i] > temperatures[st.top()]){
+                int idx = st.top();#previous index of the smaller temperature
+                st.pop();
+                ans[idx] = i -idx;
+
+            }
+           
+            st.push(i);
+
+        }
+        return ans;
+        
+    }
+};
+
